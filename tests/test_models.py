43d849559a3b5d6a38486fcb7aefefae4442dd25
AM <surhud@AM-2.local>
2022-07-12 19:05:40 +0530

    Add parameterisation mean, min, max test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -53,3 +53,27 @@ def test_daily_min_string():
     #from inflammation.models import daily_min
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])    
+
+
+
+@pytest.mark.parametrize(
+    "test_input, test_result",
+    [
+        ([[1,2],[1,3]],[1,2]),
+        ([[0,3],[1,5],[6,2]],[0,2]),
+    ])
+
+def test_daily_min2(test_input, test_result):
+    from inflammation.models import daily_min
+    npt.assert_array_equal(daily_min(np.array(test_input)),np.array(test_result))
+        
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))        
